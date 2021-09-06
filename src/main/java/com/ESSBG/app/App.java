0fc5433cd5416c4e5aa3a27d4ff7db46116802b5
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-09-06 20:38:04 +0200

    Showing a hello world window

--- src/main/java/com/ESSBG/app/App.java
+++ src/main/java/com/ESSBG/app/App.java
@@ -1,13 +1,25 @@
 package com.ESSBG.app;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+//        config.setIdleFPS(60);
+//        config.useVsync(true);
+        config.setTitle("Hello World!");
+        config.setWindowedMode(800, 600);
+        new Lwjgl3Application(new Game() {
+            @Override
+            public void create() {}
+        }, config);
     }
 }
