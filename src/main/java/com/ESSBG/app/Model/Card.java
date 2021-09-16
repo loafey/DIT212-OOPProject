3a33b295fee7a31d3dd0db7527b4b4e4e7eadd61
ember97 <71272053+ember97@users.noreply.github.com>
2021-09-16 17:19:22 +0200

    gjort möjligt för pelare att lägga till kort och rsurser på hand

--- src/main/java/com/ESSBG/app/Model/Card.java
+++ src/main/java/com/ESSBG/app/Model/Card.java
@@ -4,16 +4,21 @@ import jdk.internal.loader.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Card {
 
-    private final List<Resource> cost;
+    private final Map<String, Integer> balance;
     private final ColorEnum color;
     private final GameAction action;
 
-    public Card(List<Resource> cost, ColorEnum color, GameAction action) {
-        this.cost = cost;
+    public Card(Map<String, Integer> balance, ColorEnum color, GameAction action) {
+        this.balance = balance;
         this.color = color;
         this.action = action;
+    }
+
+    public Map<String, Integer> getBalance() {
+        return balance;
     }
 }
