3d130c0b976b0825ebdc54f0a9b86b37d17c4383
Sebastian Selander <sebastianselander@gmail.com>
2021-09-16 17:15:09 +0200

    Added attribute and methods to TrashCan

--- src/main/java/com/ESSBG/app/Model/Card.java
+++ src/main/java/com/ESSBG/app/Model/Card.java
@@ -1,17 +1,16 @@
 package com.ESSBG.app.Model;
 
-import jdk.internal.loader.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Card {
 
-    private final List<Resource> cost;
+    private final List<IResource> cost;
     private final ColorEnum color;
     private final GameAction action;
 
-    public Card(List<Resource> cost, ColorEnum color, GameAction action) {
+    public Card(List<IResource> cost, ColorEnum color, GameAction action) {
         this.cost = cost;
         this.color = color;
         this.action = action;
--- src/main/java/com/ESSBG/app/Model/Trashcan.java
+++ src/main/java/com/ESSBG/app/Model/Trashcan.java
@@ -1,4 +1,19 @@
 package com.ESSBG.app.Model;
 
+import java.util.List;
+
 public class Trashcan {
+    List<Card> cards;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public Card takeCard(int i){
+        return cards.remove(i);
+    }
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
 }
