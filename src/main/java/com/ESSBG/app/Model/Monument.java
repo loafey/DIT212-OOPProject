780ce652086444e0a0b432098289d5208cb4499c
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-09-17 01:07:58 +0200

    Getters for a Monument

--- src/main/java/com/ESSBG/app/Model/Monument.java
+++ src/main/java/com/ESSBG/app/Model/Monument.java
@@ -1,6 +1,38 @@
 package com.ESSBG.app.Model;
 
-public class Monument {
-    private String name;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Monument {
+
+    private final String name;
+    private final GameAction resource;
+    private final List<GameAction> monAction;
+    private int monUnlock;
+
+
+    public Monument(String name, GameAction resource, List<GameAction> monAction) {
+        this.name = name;
+        this.resource = resource;
+        this.monAction = monAction;
+        monUnlock = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public GameAction getResource() {
+        return resource;
+    }
+
+    // shallow copy
+    public List<GameAction> getMonAction() {
+        return new ArrayList<>(monAction);
+    }
+
+    public int getMonUnlock() {
+        return monUnlock;
+    }
 }
