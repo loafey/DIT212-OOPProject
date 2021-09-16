02406dc3151f334bb6a75bc12882d569fce8d71c
ember97 <71272053+ember97@users.noreply.github.com>
2021-09-16 16:10:12 +0200

    La till egenskaper för ett kort

--- src/main/java/com/ESSBG/app/Model/Card.java
+++ src/main/java/com/ESSBG/app/Model/Card.java
@@ -1,4 +1,19 @@
 package com.ESSBG.app.Model;
 
+import jdk.internal.loader.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card {
+
+    private final List<Resource> cost;
+    private final ColorEnum color;
+    private final GameAction action;
+
+    public Card(List<Resource> cost, ColorEnum color, GameAction action) {
+        this.cost = cost;
+        this.color = color;
+        this.action = action;
+    }
 }
