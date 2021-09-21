bddf5b62fedc60485a2152d20e5ec56b1486d23b
Gabriel <gabbehags@gmail.com>
2021-09-21 14:01:12 +0200

    Added a test class for Game and added a test for updatePlayerNeighbors

---
+++ src/test/java/com/ESSBG/app/Model/GameTest.java
@@ -0,0 +1,45 @@
+package com.ESSBG.app.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class GameTest {
+
+    Game game;
+    List<Player> testPlayers;
+
+    @Before
+    public void setup() {
+        game = new Game();
+        testPlayers = new ArrayList<>();
+        int amountTestPLayers = 3;
+        for (int i = 0; i <amountTestPLayers; i++) {
+            ResourceCard rCard0 = new ResourceCard(new HashMap<String, Integer>(), ColorEnum.BROWN, "dunno", 1);
+            ResourceCard rCard1 = new ResourceCard(new HashMap<String, Integer>(), ColorEnum.BROWN, "dunno", 1);
+            ResourceCard rCard2 = new ResourceCard(new HashMap<String, Integer>(), ColorEnum.BROWN, "dunno", 1);
+            ResourceCard rCard3 = new ResourceCard(new HashMap<String, Integer>(), ColorEnum.BROWN, "dunno", 1);
+            List<AbstractCard> monCardList = new ArrayList<>();
+            monCardList.add(rCard1);
+            monCardList.add(rCard2);
+            monCardList.add(rCard3);
+            Monument mon = new Monument("test", rCard0, monCardList);
+            testPlayers.add(new Player("testPlayer", mon));
+        }
+    }
+
+    @Test
+    public void testUpdatePlayerNeighbors() {
+        game.players.add(testPlayers.get(0));
+        game.players.add(testPlayers.get(1));
+        game.players.add(testPlayers.get(2));
+        game.updatePlayerNeighbors();
+        assertEquals(game.players.get(0).getLeftPlayer(), testPlayers.get(2));
+        assertEquals(game.players.get(0).getRightPlayer(), testPlayers.get(1));
+    }
+}
