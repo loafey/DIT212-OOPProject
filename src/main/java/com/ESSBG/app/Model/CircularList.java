688f0e4c2fb9030b8c7a86b95a5ff48971a57950
Gabriel <gabbehags@gmail.com>
2021-09-21 13:28:37 +0200

    CircularList now extends ArrayList, so we can get a lot of methods for free and the old implementation was using ArrayList so no methods were lost

--- src/main/java/com/ESSBG/app/Model/CircularList.java
+++ src/main/java/com/ESSBG/app/Model/CircularList.java
@@ -1,55 +1,35 @@
 package com.ESSBG.app.Model;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class CircularList <T>{
-    private List<T> list;
+public class CircularList<T> extends ArrayList<T>{
 
     public CircularList(){
-        list = new ArrayList<>();
+        super();
     }
-
-
-    public void add (T t){
-        list.add(t);
-    }
-
-    public void remove(T t){
-        list.remove(t);
-    }
-
-    public T get(int i){
-        return list.get(i);
-    }
-
-    public int size(){
-        return list.size();
-    }
-
 
     public T getNext(T t){
-        int i = list.indexOf(t);
+        int i = super.indexOf(t);
 
         if (i == -1){
             throw new IllegalArgumentException("Element cannot be found in list");
         }
-        if (i == list.size() - 1 ){
-            return list.get(0);
+        if (i == super.size() - 1 ){
+            return super.get(0);
         }
         else{
-            return list.get(i+1);
+            return super.get(i+1);
         }
     }
 
     public T getPrevious(T t){
-        int i = list.indexOf(t);
+        int i = super.indexOf(t);
 
         if (i == 0){
-            return list.get(list.size()-1);
+            return super.get(super.size()-1);
         }
         else{
-            return list.get(i-1);
+            return super.get(i-1);
         }
     }
 }
