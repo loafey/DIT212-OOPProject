879474f764d9df07b623a6b109fc4c97a3b32525
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-09-26 19:28:15 +0200

    Clearing the Depth buffer

--- src/main/java/com/ESSBG/app/Render/GameScene/GameScene.java
+++ src/main/java/com/ESSBG/app/Render/GameScene/GameScene.java
@@ -80,7 +80,7 @@ public class GameScene implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.25f, 0.25f, 0.25f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         stage.act(delta);
         stage.draw();
