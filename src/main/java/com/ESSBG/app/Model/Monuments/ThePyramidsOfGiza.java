d621d41a40fbd3356c74f35c725c53277df4bf04
ember97 <71272053+ember97@users.noreply.github.com>
2021-09-30 10:56:37 +0200

    Skapade ett till monument

---
+++ src/main/java/com/ESSBG/app/Model/Monuments/ThePyramidsOfGiza.java
@@ -0,0 +1,38 @@
+package com.ESSBG.app.Model.Monuments;
+
+import com.ESSBG.app.Model.Player.Player;
+import com.ESSBG.app.Model.ResourceEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThePyramidsOfGiza extends Monument{
+
+    public ThePyramidsOfGiza(Player player) {
+        super("ThePyramidsOfGiza",player, ResourceEnum.STONE);
+    }
+
+    @Override
+    public void stage2Reward() {
+        List<ResourceEnum> guaranteedResources = player.getState().getGuaranteedResources();
+        for (int i=0; i<5; i++) {
+            guaranteedResources.add(ResourceEnum.POINT);
+        }
+        player.getState().setGuaranteedResources(guaranteedResources);
+    }
+
+    @Override
+    protected void initResourcesToBuildStage1() {
+        resourcesToBuildStage1 = super.initializeResources(ResourceEnum.STONE, 2);
+    }
+
+    @Override
+    protected void initResourcesToBuildStage2() {
+        resourcesToBuildStage1 = super.initializeResources(ResourceEnum.WOOD, 3);
+    }
+
+    @Override
+    protected void initResourcesToBuildStage3() {
+        resourcesToBuildStage1 = super.initializeResources(ResourceEnum.STONE, 4);
+    }
+}
