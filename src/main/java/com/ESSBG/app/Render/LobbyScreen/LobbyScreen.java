7ad05d635b3114cad8bfaac34479a0fafe634a87
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-09-30 11:19:21 +0200

    Added some basic buttons to the lobby screen.

--- src/main/java/com/ESSBG/app/Render/LobbyScreen/LobbyScreen.java
+++ src/main/java/com/ESSBG/app/Render/LobbyScreen/LobbyScreen.java
@@ -3,9 +3,13 @@ package com.ESSBG.app.Render.LobbyScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -15,6 +19,9 @@ public class LobbyScreen implements Screen{
     private Skin skin;
     private Table sceneTable;
 
+    private TextField ipField;
+    private Button joinButton;
+
     public LobbyScreen(Game game){
         this.game = game;
     }
@@ -23,16 +30,35 @@ public class LobbyScreen implements Screen{
     public void show() {
         Viewport vp = new ScreenViewport();
         stage = new Stage(vp);
-        skin = new Skin(Gdx.files.internal("Assets/Skins/LobbyScreen/LobbyScreenSkin.json"));
+        skin = new Skin(Gdx.files.internal("Assets/Skins/LobbyScreen/LobbySceneSkin.json"));
         
         sceneTable = new Table();
+        sceneTable.center();
+        sceneTable.setFillParent(true);
         stage.addActor(sceneTable);
+
+        Gdx.input.setInputProcessor(stage);
+        
+        sceneTable.add(new Label("Join Server:",skin));
+        sceneTable.row();
+        joinButton = new Button(skin);
+        joinButton.add(new Label("Connect:", skin));
+        sceneTable.add(joinButton);
+
+        ipField = new TextField("", skin);
+        sceneTable.add(ipField);
     }
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0.25f, 0.25f, 0.25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
     }
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
     @Override
     public void pause() {
@@ -45,6 +71,7 @@ public class LobbyScreen implements Screen{
     }
     @Override
     public void dispose() {
+        stage.dispose();
     }
     
 }
