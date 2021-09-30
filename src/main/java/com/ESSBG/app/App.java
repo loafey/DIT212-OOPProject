b0926321305b554fc941cf8df58b6659f483c600
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-09-30 23:53:05 +0200

    Deleted MainGame that extended Game and replaced it with MainScreen that implements ApplicationListener

--- src/main/java/com/ESSBG/app/App.java
+++ src/main/java/com/ESSBG/app/App.java
@@ -1,6 +1,6 @@
 package com.ESSBG.app;
 
-import com.ESSBG.app.Render.MainGame;
+import com.ESSBG.app.Render.MainScreen;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -9,6 +9,6 @@ public class App {
     public static void main( String[] args ) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setMaximized(true);
-        new Lwjgl3Application(new MainGame(), config);
+        new Lwjgl3Application(new MainScreen(), config);
     }
 }
