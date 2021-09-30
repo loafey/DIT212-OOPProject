35cda3d43e9f992adcf6ed239bf2a2794504a51a
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-10-01 00:20:07 +0200

    Documented

--- src/main/java/com/ESSBG/app/Render/ScreenManager.java
+++ src/main/java/com/ESSBG/app/Render/ScreenManager.java
@@ -3,6 +3,9 @@ package com.ESSBG.app.Render;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 
+/**
+ * ScreenManager is a singleton that have the ability to change the current screen or get the instance of the current screen.
+ */
 public class ScreenManager {
 
     private Screen current;
@@ -10,21 +13,36 @@ public class ScreenManager {
 
     private ScreenManager() {}
 
+    // Helper class that is the holder of this instance
     private static class ScreenManagerHolder {
         public static final ScreenManager instance = new ScreenManager();
     }
 
+    /**
+     * Returns the only instance of the {@link ScreenManager}.
+     *
+     * @return the instance of {@link ScreenManager}
+     */
     public static ScreenManager getInstance() {
         return ScreenManagerHolder.instance;
     }
 
 
+    /**
+     * Switches the screen back to the previous {@link Screen}.
+     */
     public void goBackScreen() {
         if (last != null) {
             setScreen(last);
         }
     }
 
+
+    /**
+     * Sets the screen if screen is not null.
+     *
+     * @param screen the screen that will be displayed
+     */
     public void setScreen(Screen screen) {
         if (current != null) {
             current.hide();
@@ -37,6 +55,12 @@ public class ScreenManager {
         }
     }
 
+
+    /**
+     * Return the current screen if a screen have been set before, else return null.
+     *
+     * @return the current {@link Screen}
+     */
     public Screen getScreen() {
         return current;
     }
