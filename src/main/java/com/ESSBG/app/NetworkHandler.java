348750ac926df6ce2d1d2ad5d6fb0ca2a7378555
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-10-05 16:58:50 +0200

    Added a NetworkHandler that should remove the network responsibility from Game.

---
+++ src/main/java/com/ESSBG/app/NetworkHandler.java
@@ -0,0 +1,12 @@
+package com.ESSBG.app;
+
+import com.ESSBG.app.Model.GameNetMessageListener;
+import com.ESSBG.app.Network.Client;
+
+public class NetworkHandler {
+    private Thread serverThread;
+    private Client client;    
+
+    private void startServer(){
+    }
+}
