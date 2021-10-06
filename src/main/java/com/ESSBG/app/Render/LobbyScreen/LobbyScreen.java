ede0abb4064b70e243ceec125cd9830cdd6541d7
Sebastian Selander <sebastianselander@gmail.com>
2021-10-06 12:45:05 +0200

    Added ResourceActionTest

--- src/main/java/com/ESSBG/app/Render/LobbyScreen/LobbyScreen.java
+++ src/main/java/com/ESSBG/app/Render/LobbyScreen/LobbyScreen.java
@@ -4,14 +4,12 @@ import com.ESSBG.app.Model.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
