5fe10355f96422cd6bac354326ee5cb54cced482
ember97 <71272053+ember97@users.noreply.github.com>
2021-10-07 13:13:37 +0200

    Dokumentation

--- src/main/java/com/ESSBG/app/Model/Player/Player.java
+++ src/main/java/com/ESSBG/app/Model/Player/Player.java
@@ -20,17 +20,11 @@ public class Player {
     private Player leftNeighbor;    //remove, player shouldn't hold his own neighbors
     private Player rightNeighbor;
 
-
-    public Player(int id, PlayerState state, Player leftNeighbor, Player rightNeighbor) {
-        this.id = id;
-        this.name = String.valueOf(id);
-        this.state = state;
-        this.leftNeighbor = leftNeighbor;
-        this.rightNeighbor = rightNeighbor;
-        this.eitherActionHandlers = new ArrayList<>();
-        this.neighborReductionHandlers = new ArrayList<>();
-        this.resourceActionHandlers = new ArrayList<>();
-    }
+    /**
+     * The constructor for a Player. Creates a player with an id, a state and initializes it's neighbors.
+     * @param id Unique id-number for each player
+     * @param state A player state that holds this player's resources and cards
+     */
 
     public Player(int id, PlayerState state) {
         this.id = id;
@@ -41,6 +35,11 @@ public class Player {
         this.resourceActionHandlers = new ArrayList<>();
     }
 
+    /**
+     * Determines if two players are equal
+     * @param o
+     * @return true if same id, false if not
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null) {
@@ -50,42 +49,50 @@ public class Player {
         return p.getId() == this.id;
     }
 
+    /**
+     * Getter for id
+     * @return this player's id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter for name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for state
+     * @return a safe copy of this player's state
+     */
     public PlayerState getState() {
         return new PlayerState(state);
     }
 
+    /**
+     * Setter for state
+     * @param state
+     */
     public void setState(PlayerState state) {
         this.state = new PlayerState(state);
     }
 
-    public Player getLeftNeighbor() {
-        return leftNeighbor;
-    }
-
-    public Player getRightNeighbor() {
-        return rightNeighbor;
-    }
-
-    public void setLeftNeighbor(Player p) {
-        this.leftNeighbor = p;
-    }
-
-    public void setRightNeighbor(Player p) {
-        this.rightNeighbor = p;
-    }
-
+    /**
+     * Setter for monument
+     * @param monument
+     */
     public void setMonument(Monument monument) {
         this.monument = monument;
     }
 
+    /**
+     * Getter for monument
+     * @return
+     */
     public Monument getMonument() {
         return monument;
     }
@@ -118,7 +125,10 @@ public class Player {
         this.getState().setGuaranteedResources(list);
     }
 
-
+    /**
+     * Builds a stage of this player's monument if the player has efficient resources to do so
+     * @return true if build successful, false if not
+     */
     public boolean buildStageOfMonument(){
         boolean playerHasEfficientResources = false;    //TODO  Vet ej hur man ska kolla det
 
@@ -144,7 +154,7 @@ public class Player {
     }
 
     /**
-     * Initialize the starting amount of coins and the play'ers monument's starting resource
+     * Initialize the starting amount of coins and the player's monument's starting resource
      */
     public void initResources(){
         List<ResourceEnum> list = state.getGuaranteedResources();
