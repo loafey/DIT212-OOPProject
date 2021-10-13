3dc85845a42c698596972e7991ca42e6071529f7
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-10-13 13:06:07 +0200

    Made the cost static.

--- src/main/java/com/ESSBG/app/Model/Cards/CardFactory.java
+++ src/main/java/com/ESSBG/app/Model/Cards/CardFactory.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Random;
 
 public class CardFactory {
-    private final List<ResourceEnum> noCost = new ArrayList<>();
+    private static final List<ResourceEnum> noCost = new ArrayList<>();
 
     private CardFactory(){
     }
