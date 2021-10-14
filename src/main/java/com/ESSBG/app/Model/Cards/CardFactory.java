0c7f67f77f4eca8b7e8431f058c07b14de125dfd
ember97 <71272053+ember97@users.noreply.github.com>
2021-10-14 15:04:01 +0200

    First version of CardFactory, will be extended with more tests

--- src/main/java/com/ESSBG/app/Model/Cards/CardFactory.java
+++ src/main/java/com/ESSBG/app/Model/Cards/CardFactory.java
@@ -1,5 +1,7 @@
 package com.ESSBG.app.Model.Cards;
 
+import com.ESSBG.app.Model.Action.EitherResourceAction;
+import com.ESSBG.app.Model.Action.NeighborReductionAction;
 import com.ESSBG.app.Model.Action.ResourceAction;
 import com.ESSBG.app.Model.ResourceEnum;
 
@@ -7,14 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static java.util.Collections.shuffle;
+
 public class CardFactory {
+    // Only for testing purposes
+    public static final int nrOfWarCards = 8;
+    public static final int nrOfCoinCards = 3;
+    public static final int nrOfEitherCards = 6;
+    public static final int nrOfNeighborRedCards = 6;
+    public static final int nrOfGreenCards = 6;
+    public static final int nrOfBasicResCards = 8;
+    public static final int nrOfBasicLuxuryResCards = 6;
+    public static final int nrOfVicPointsCards = 6;
+
     private static final List<ResourceEnum> noCost = new ArrayList<>();
 
-    private CardFactory(){
+    private CardFactory() {
     }
 
-    public static List<List<Card>> getPeriodCards() {
-        return null;
+    public static List<List<Card>> generateHands(int age, int playerAmount, int handSize) {
+        CardFactory cardFactory = new CardFactory();
+        return cardFactory.getPeriodCards(age, playerAmount, handSize);
     }
 
     /**
@@ -25,58 +40,112 @@ public class CardFactory {
      * @param handSize     The size of a hand
      * @return all cards
      */
-    public static List<List<Card>> generateHands(int age, int playerAmount, int handSize) {
-        ArrayList<List<Card>> allCards = new ArrayList<>(playerAmount);
-        for (int i = 0; i < playerAmount; i++) {
-            ArrayList<Card> hand = new ArrayList<>(handSize);
-            for (int j = 0; j < handSize; j++) {
-                hand.add(generateCards(age).get(j));
+    //TODO funkar ej
+    public List<List<Card>> getPeriodCards(int age, int playerAmount, int handSize) {
+        List<List<Card>> playerHands = new ArrayList<>();
+        List<Card> allCards = generateCards(age);
+        for (int i=0; i<7; i++){
+            List<Card> tmp = new ArrayList<>();
+            for (int j=0; j<7; j++){
+                Card c = allCards.get(j+i*7);
+                tmp.add(c);
             }
-            allCards.add(hand);
+            playerHands.add(tmp);
         }
-        return allCards;
+        return playerHands;
     }
 
-
-    public static List<Card> generateCards(int age) {
+    /**
+     * Generates a shuffled list of 49 cards for a certain age
+     * @param age
+     * @return the shuffled list with 49 cards
+     */
+    public List<Card> generateCards(int age) {
         List<Card> cards = new ArrayList<>();
         List<ResourceEnum> basicResources = getAllBasicResources();
-        int nrOfResourceCopies = 2;
-        int nrOfResources = age;
+        int nrOfResourcesOnCard = age;
         int nrOfWarPointsOnCard = age;
-        int nrOfWarCards = 2 * age + 4;
-        int nrOfCoinsOnCard = 3 + 2 * age;
-        int nrOfCoinCards = age * 3;
+        int nrOfCoinsOnCard = 5;
 
-        // Basic resources
+
+
+
+        // Basic resources, two of each kind (8 cards totally)
         for (ResourceEnum r : getAllBasicResources()) {
-            cards.add(new ResourceActionCard("Resource", noCost, ColorEnum.BROWN, new ResourceAction(getListOfResources(nrOfResources, r))));
-
+            cards.add(new ResourceActionCard("Resource", new ArrayList<>(noCost), ColorEnum.BROWN, new ResourceAction(getListOfResources(nrOfResourcesOnCard, r))));
+            cards.add(new ResourceActionCard("Resource", new ArrayList<>(noCost), ColorEnum.BROWN, new ResourceAction(getListOfResources(nrOfResourcesOnCard, r))));
         }
-        // Luxury resources
+        // Luxury resources, two of each kind (6 cards totally)
         for (ResourceEnum r : getAllLuxuryResources()) {
-            cards.add(new ResourceActionCard("Resource", noCost, ColorEnum.GRAY, new ResourceAction(getListOfResources(nrOfResources, r))));
-
+            cards.add(new ResourceActionCard("Resource", new ArrayList<>(noCost), ColorEnum.GRAY, new ResourceAction(getListOfResources(nrOfResourcesOnCard, r))));
+            cards.add(new ResourceActionCard("Resource", new ArrayList<>(noCost), ColorEnum.GRAY, new ResourceAction(getListOfResources(nrOfResourcesOnCard, r))));
         }
 
         // War
         for (int i = 0; i < nrOfWarCards; i++) {
-            cards.add(new ResourceActionCard("War wagon", noCost, ColorEnum.RED, new ResourceAction(getListOfResources(nrOfWarPointsOnCard, ResourceEnum.WAR))));
+            cards.add(new ResourceActionCard("War wagon", new ArrayList<>(noCost), ColorEnum.RED, new ResourceAction(getListOfResources(nrOfWarPointsOnCard, ResourceEnum.WAR))));
         }
 
         // Coins
         for (int i = 0; i < nrOfCoinCards; i++) {
-            cards.add(new ResourceActionCard("Treasure", noCost, ColorEnum.YELLOW, new ResourceAction(getListOfResources(nrOfCoinsOnCard, ResourceEnum.COIN))));
+            cards.add(new ResourceActionCard("Treasure", new ArrayList<>(noCost), ColorEnum.YELLOW, new ResourceAction(getListOfResources(nrOfCoinsOnCard, ResourceEnum.COIN))));
         }
 
 
         // Victory points
-        // Either resources
-        // Neighbor red resources
-        // Lib, lab, disp
+        for (int i = 0; i < 3; i++) {
+            cards.add(new ResourceActionCard("Theater", new ArrayList<>(noCost), ColorEnum.BLUE, new ResourceAction(getListOfResources(age + 1, ResourceEnum.POINT))));
+        }
+        for (int i = 0; i < 2; i++) {
+            cards.add(new ResourceActionCard("Baths", new ArrayList<>(noCost), ColorEnum.BLUE, new ResourceAction(getListOfResources(age + 2, ResourceEnum.POINT))));
+        }
 
+        cards.add(new ResourceActionCard("Pawn shop", getListOfResources(1, ResourceEnum.STONE), ColorEnum.BLUE, new ResourceAction(getListOfResources(age + 1, ResourceEnum.POINT))));
+
+
+        // Either resources
+        cards.add(new EitherResourceCard("Timber yard", new ArrayList<>(noCost), ColorEnum.BROWN, new EitherResourceAction(getListOfEitherResources(ResourceEnum.WOOD, ResourceEnum.STONE))));
+        cards.add(new EitherResourceCard("Clay pit", new ArrayList<>(noCost), ColorEnum.BROWN, new EitherResourceAction(getListOfEitherResources(ResourceEnum.CLAY, ResourceEnum.ORE))));
+        cards.add(new EitherResourceCard("Mine", new ArrayList<>(noCost), ColorEnum.BROWN, new EitherResourceAction(getListOfEitherResources(ResourceEnum.STONE, ResourceEnum.ORE))));
+
+        cards.add(new EitherResourceCard("Caravansery", getListOfResources(2, ResourceEnum.WOOD), ColorEnum.YELLOW, new EitherResourceAction(getAllBasicResources())));
+        cards.add(new EitherResourceCard("Caravansery", getListOfResources(2, ResourceEnum.WOOD), ColorEnum.YELLOW, new EitherResourceAction(getAllBasicResources())));
+        cards.add(new EitherResourceCard("Forum", getListOfResources(2, ResourceEnum.CLAY), ColorEnum.YELLOW, new EitherResourceAction(getAllLuxuryResources())));
+
+
+        // Neighbor red resources
+        for (int i=0; i<nrOfNeighborRedCards/2; i++){
+            cards.add(new NeighborReductionCard("Trading post", new ArrayList<>(noCost), ColorEnum.YELLOW, new NeighborReductionAction(getAllBasicResources())));
+            cards.add(new NeighborReductionCard("Market place", new ArrayList<>(noCost), ColorEnum.YELLOW, new NeighborReductionAction(getAllLuxuryResources())));
+        }
+
+        // Lib, lab, disp
+        for (int i=0; i<nrOfGreenCards/3; i++){
+            cards.add(new ResourceActionCard("Library", new ArrayList<>(noCost), ColorEnum.GREEN, new ResourceAction(getListOfResources(1, ResourceEnum.Library))));
+            cards.add(new ResourceActionCard("Laboratory", new ArrayList<>(noCost), ColorEnum.GREEN, new ResourceAction(getListOfResources(1, ResourceEnum.Laboratory))));
+            cards.add(new ResourceActionCard("Dispensary", new ArrayList<>(noCost), ColorEnum.GREEN, new ResourceAction(getListOfResources(1, ResourceEnum.Dispensary))));
+        }
+
+        //shuffle(cards);
         return cards;
     }
+
+    /**
+     * Get a list of specific resources
+     * @param r1 one resource
+     * @param r2 another resource
+     * @return list of r1 and r2
+     */
+
+    private List<ResourceEnum> getListOfEitherResources(ResourceEnum r1, ResourceEnum r2){
+        List<ResourceEnum> list = new ArrayList<>();
+        list.add(r1);
+        list.add(r2);
+        return list;
+    }
+
+
+
 
 
     /**
@@ -85,7 +154,7 @@ public class CardFactory {
      * @param r resource
      * @return color for the resource
      */
-    private static ColorEnum getColor(ResourceEnum r) {
+    private ColorEnum getColor(ResourceEnum r) {
         if (r.equals(ResourceEnum.WAR)) {
             return ColorEnum.RED;
         } else if (r.equals(ResourceEnum.POINT)) {
@@ -100,11 +169,11 @@ public class CardFactory {
     }
 
     /**
-     * Generate a list of all available resources in the game
+     * Generate a list of all available basic resources in the game
      *
      * @return
      */
-    private static List<ResourceEnum> getAllBasicResources() {
+    private List<ResourceEnum> getAllBasicResources() {
         List<ResourceEnum> list = new ArrayList<>();
         list.add(ResourceEnum.WOOD);
         list.add(ResourceEnum.CLAY);
@@ -114,11 +183,11 @@ public class CardFactory {
     }
 
     /**
-     * Generate a list of all available resources in the game
+     * Generate a list of all available luxury resources in the game
      *
      * @return
      */
-    private static List<ResourceEnum> getAllLuxuryResources() {
+    private List<ResourceEnum> getAllLuxuryResources() {
         List<ResourceEnum> list = new ArrayList<>();
 
         list.add(ResourceEnum.PAPYRUS);
@@ -134,7 +203,7 @@ public class CardFactory {
      * @param resource the kind of resource you want to have
      * @return
      */
-    private static List<ResourceEnum> getListOfResources(int amount, ResourceEnum resource) {
+    private List<ResourceEnum> getListOfResources(int amount, ResourceEnum resource) {
         List<ResourceEnum> resources = new ArrayList<>();
 
         for (int i = 0; i < amount; i++) {
--- src/test/java/com/ESSBG/app/Model/Card/CardFactoryTest.java
+++ src/test/java/com/ESSBG/app/Model/Card/CardFactoryTest.java
@@ -2,7 +2,9 @@ package com.ESSBG.app.Model.Card;
 
 import com.ESSBG.app.Model.Cards.Card;
 import com.ESSBG.app.Model.Cards.CardFactory;
+import com.ESSBG.app.Model.Cards.ColorEnum;
 import com.ESSBG.app.Model.Cards.ResourceActionCard;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,25 +13,65 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CardFactoryTest {
-    List<List<Card>> cardList = CardFactory.generateHands(1, 3, 7);
+    List<List<Card>> cardList;
+    @Before
+    public void init(){
+        cardList = CardFactory.generateHands(1, 7, 7);
+    }
 
     @Test
     public void testCorrectAmountOfCards(){
-        assertEquals(3, cardList.size());
+        int count = 0;
 
-        for (List<Card> list : cardList){
-            assertEquals(7, list.size());
-        }
-    }
 
-    //TODO kolla så resource inte är null, kan inte just nu utan att ändra en del
-    @Test
-    public void testValidResourceCards(){
         for (List<Card> list : cardList){
             for (Card card : list){
-                assertNotNull(card.getColor());
-                assertNotNull(card.getCardTypeEnum());
+                count++;
             }
         }
+
+        assertEquals(49, count);
+    }
+
+
+    @Test
+    public void testCorrectAmountOfColorCards(){
+        int countBrown = 0;
+        int countGreen = 0;
+        int countRed = 0;
+        int countYellow = 0;
+        int countGrey = 0;
+        int countBlue = 0;
+
+        for (List<Card> cards : cardList){
+            for (Card c : cards){
+                if (c.getColor() == ColorEnum.BROWN){
+                    countBrown++;
+                }
+                else if (c.getColor() == ColorEnum.GREEN){
+                    countGreen++;
+                }
+                else if (c.getColor() == ColorEnum.RED){
+                    countRed++;
+                }
+                else if (c.getColor() == ColorEnum.BLUE){
+                    countBlue++;
+                }
+                else if (c.getColor() == ColorEnum.GRAY){
+                    countGrey++;
+                }
+                else if (c.getColor() == ColorEnum.YELLOW){
+                    countYellow++;
+                }
+            }
+        }
+
+
+        assertEquals(CardFactory.nrOfBasicResCards + CardFactory.nrOfEitherCards/2, countBrown);
+        assertEquals(CardFactory.nrOfBasicLuxuryResCards, countGrey);
+        assertEquals(CardFactory.nrOfCoinCards + CardFactory.nrOfNeighborRedCards + CardFactory.nrOfEitherCards/2, countYellow);
+        assertEquals(CardFactory.nrOfWarCards, countRed);
+        assertEquals(CardFactory.nrOfGreenCards, countGreen);
+        assertEquals(CardFactory.nrOfVicPointsCards, countBlue);
     }
 }
